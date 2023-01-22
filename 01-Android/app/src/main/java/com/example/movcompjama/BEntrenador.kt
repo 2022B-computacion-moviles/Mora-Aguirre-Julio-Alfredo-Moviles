a52662882c0006b787e6f29620e7e6787fd47c24
Julio Mora Aguirre <juliomorab2@hotmail.com>
2023-01-22 12:53:47 -0500

    Commit List View Parte 2 y Intents Explicitos/Implitictos Parte 1
    Commit 09-01-2023

--- 01-Android/app/src/main/java/com/example/movcompjama/BEntrenador.kt
+++ 01-Android/app/src/main/java/com/example/movcompjama/BEntrenador.kt
@@ -2,6 +2,12 @@ package com.example.movcompjama
 
 class BEntrenador (
     var nombre:String,
-    var descripcion:String  ){
+    var descripcion:String
+
+    ){
+
+    fun tostring(): String {
+        return "{$nombre} - ${descripcion}"
+    }
 
 }
