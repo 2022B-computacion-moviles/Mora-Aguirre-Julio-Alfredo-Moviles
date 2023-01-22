c0120add3eec1e33b403d0f667deb96818e8674b
Julio Mora Aguirre <juliomorab2@hotmail.com>
2023-01-22 13:43:25 -0500

    Commit Intentents Explicito Parte 2
    devolver respuesta 10 - 01 - 2023

--- 01-Android/app/src/main/java/com/example/movcompjama/CIntentExplicitoParametros.kt
+++ 01-Android/app/src/main/java/com/example/movcompjama/CIntentExplicitoParametros.kt
@@ -1,11 +1,33 @@
 package com.example.movcompjama
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CIntentExplicitoParametros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cintent_explicito_parametros)
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val edad = intent.getIntExtra("edad", 0)
+
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton
+            .setOnClickListener { devolverRespuesta() }
+    }
+
+    fun devolverRespuesta(){
+        val intentoDevovlerParametros = Intent()
+        intentoDevovlerParametros.putExtra("nombreModificado", "Alfredo")
+        intentoDevovlerParametros.putExtra("edadModificada", 25)
+        // this.result
+        setResult(
+            RESULT_OK,
+            intentoDevovlerParametros
+        )
+        // this.finish()
+        finish()
     }
 }
--- 01-Android/app/src/main/java/com/example/movcompjama/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movcompjama/MainActivity.kt
@@ -1,14 +1,60 @@
 package com.example.movcompjama
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    val contenidoIntentExplicito =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (result.data != null){
+                    val data = result.data
+                    Log.i("intent-epn", "${data?.getStringExtra("nombreModificado")}")
+                }
+            }
+        }
+
+    val contenidoIntentImplicito =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (result.data != null){
+                    if (result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null
+                        )
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(
+                            indiceTelefono!!
+                        )
+                        cursor?.close()
+                        Log.i("intent-epn", "Telefono ${telefono}")
+                    }
+                }
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
             .setOnClickListener{
@@ -17,16 +63,47 @@ class MainActivity : AppCompatActivity() {
 
         ///Boton para list view de entrenadores
 
-
         val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
         botonListView
             .setOnClickListener{
                 irActividad(BListView::class.java)
             }
 
+        // boton para el intent implicito
 
+        val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonIntentImplicito
+            .setOnClickListener {
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                contenidoIntentImplicito.launch(intentConRespuesta)
+            }
+
+        val botonIntent = findViewById<Button>(R.id.btn_intent)
+        botonIntent
+            .setOnClickListener {
+                abrirActividadConParamatros(CIntentExplicitoParametros::class.java)
+            }
 
     }
+
+    //abrir actividad con parametros
+
+    fun abrirActividadConParamatros(
+        clase: Class<*>,
+    ) {
+        val intentExplicito = Intent(this, clase)
+        // Enviar parámetros (solamente variables primitivas)
+        intentExplicito.putExtra("nombre", "Julio")
+        intentExplicito.putExtra("apellido", "Mora")
+        intentExplicito.putExtra("edad", 21)
+
+        contenidoIntentExplicito.launch(intentExplicito)
+    }
+
+
     fun irActividad(
         clase: Class<*>
     ){
